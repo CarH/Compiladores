638dca66fb968070dc17a4ccd30a89793f842ed4
bssanches <brunosanches94@gmail.com>
2015-04-18 12:12:29 -0300

    script dos fera

--- Projeto_1(Analise_lexica)/script.sh
+++ Projeto_1(Analise_lexica)/script.sh
@@ -1,12 +1,19 @@
 echo "Digite o nome do arquivo de entrada (Exemplo: bla.c)"
 read x;
+y=$(ls $x)
+if [ "$y" == "" ]; then
+	echo "FILE NOT FOUND"
+	exit 0;
+fi
 
-echo "Compilando o compiler"
+echo "Compilando o compiler..."
 lex myscanner.l
 gcc lex.yy.c myscanner.c -o scanner -g
 
-echo "Rodando analise"
+echo "Rodando analise..."
 ./scanner < $x > result.out
+
+
 
 cat result.out
 
